7b98fd4ba7bc4e60d81583915bcfd799fbca6af0
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-20 16:21:07 +0900

    margin 調整

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -2,11 +2,14 @@ export default function Header() {
   return (
     <>
       <header>
-        <h1>AIデートプラン提案アプリ</h1>
+        <h1 className="title">AIデートプラン提案アプリ</h1>
       </header>
       <style jsx>{`
         header {
           background-color: pink;
+          .title {
+            margin: 0;
+          }
         }
       `}</style>
     </>
