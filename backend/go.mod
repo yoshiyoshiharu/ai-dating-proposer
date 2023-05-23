e6f8587f65299de8d1561767c4759c4c90fbea49
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-24 08:37:41 +0900

    Goでopenaiを叩く

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module githu.com/yoshiyoshiharu/ai-dating-proposer
 
 go 1.20
+
+require github.com/sashabaranov/go-openai v1.9.4 // indirect
---
+++ backend/openai/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	fetchPlans()
+}
+
+func fetchPlans() {
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
