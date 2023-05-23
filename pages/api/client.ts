cc4dc511b7f27b8dc99c4b5731c46719baa50180
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-23 22:22:35 +0900

    不要な条件削除

--- pages/api/client.ts
+++ pages/api/client.ts
@@ -79,6 +79,6 @@ NOTES:
 * Please do not include anything other than JSON in your answer.
 * Response must be Japanese
 
-${area} 湾岸 What 5 dating plan do you propose?
+${area} What 5 dating plan do you propose?
 `
 }
