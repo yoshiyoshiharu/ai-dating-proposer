e1db8d0d0a08b0c399f0bdb142ec36a9723b6c2f
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-26 21:30:26 +0900

    tmpに画像生成

--- backend/google_map/client.go
+++ backend/google_map/client.go
@@ -48,9 +48,7 @@ func FetchPlaceDetail() {
 
 func FetchPhoto() {
   client, err := maps.NewClient(maps.WithAPIKey(os.Getenv("GOOGLE_API_KEY")))
-	if err != nil {
-		log.Fatalf("fatal error: %s", err)
-	}
+	check(err)
 
 	r := &maps.PlacePhotoRequest{
 		PhotoReference: "AZose0m0Wawrjo-vbQqjgEcdRAC41M1izkkOBYkOO3BtzNH_KavGGIHnXcQnYLJaZ_Eh4ahTKa4kffXnnU5_RO43bQ4LMAY9JrS3SbZqhBt5q6-TlaCXscXQaMVjPoq8DwQZzJcyvhjjRbIzrEoEVC4sPaXq9j_V7Zz7OIwk9hTx2OVdkxxw",
@@ -66,7 +64,7 @@ func FetchPhoto() {
 	img, err := resp.Image()
 	check(err)
 
-	file, err := os.Create("photo.jpg")
+	file, err := os.Create("tmp/photo.jpg")
 	check(err)
 	err = jpeg.Encode(file, img, &jpeg.Options{Quality: 100})
 	check(err)
