d6479045d06213fa38448c6c4ab1b89438a22720
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-29 23:05:03 +0900

    fix icon

--- frontend/pages/components/Header.tsx
+++ frontend/pages/components/Header.tsx
@@ -4,14 +4,14 @@ export default function Header() {
   return (
     <>
       <header>
-        <Image src="/header.png" alt="icon" className="header-icon" width="64" height="64" />
+        <Image src="/icon2.png" alt="icon" className="header-icon" width="64" height="64" />
         <h1 className="title">
           AIデートプラン提案アプリ
         </h1>
       </header>
       <style jsx>{`
         header {
-          background-color: pink;
+          background-color: #ffaaaa;
           display: flex;
           align-items: center;
           .title {
