13f748c4c2375b3a2d198ffa431bd1c77c21aa49
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-29 20:32:16 +0900

    プランが見つからなかったときのメッセージを表示する

--- frontend/pages/components/Cards.tsx
+++ frontend/pages/components/Cards.tsx
@@ -2,43 +2,56 @@ import { Plan } from '../../entity/plan';
 import Image from './Image';
 
 const Cards = ({ plans }: { plans: Plan[] }) => {
-  return (
-    <>
-      <div className="cards">
-        {
-          plans.length == 0 || plans.map((plan) => (
-            <div className="card" key={plan.place}>
-              <h2>{plan.place}</h2>
-              <div className="photos">
-                {
-                  plan.photo_references?.map((photoReference: string) => (
-                    <Image photoReference={photoReference}></Image>
-                  ))
-                }
-              </div>
-            </div>
-          ))
-        }
-      </div>
+  if (plans.length == 0) {
+    return (
+      <>
+        <h2>プランが見つかりませんでした。もう一度試してください。</h2>
       <style jsx>{`
-        .cards {
-          .card {
-            width: 50%;
-            background-color: #fff;
-            margin: 10px auto;
-            padding: 10px;
-            border: 1px solid #333;
-            border-radius: 10px;
-          }
-        }
-        .photos {
-          display: flex;
-          width: 100%;
-          overflow: scroll;
-        }
+         h2 {
+            text-align: center;
+         }
       `}</style>
-    </>
-  )
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className="cards">
+          {
+            plans.map((plan) => (
+              <div className="card" key={plan.place}>
+                <h2>{plan.place}</h2>
+                <div className="photos">
+                  {
+                    plan.photo_references?.map((photoReference: string) => (
+                      <Image photoReference={photoReference}></Image>
+                    ))
+                  }
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <style jsx>{`
+          .cards {
+            .card {
+              width: 50%;
+              background-color: #fff;
+              margin: 10px auto;
+              padding: 10px;
+              border: 1px solid #333;
+              border-radius: 10px;
+            }
+          }
+          .photos {
+            display: flex;
+            width: 100%;
+            overflow: scroll;
+          }
+        `}</style>
+      </>
+    )
+  }
 }
 
 export default Cards;
