25b6ca9a701f002ecf649dadd571429a8581c7a4
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-05-30 19:28:40 +0900

    mainでbing APIを叩く

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,10 +1,57 @@
 package main
 
 import (
-	"github.com/yoshiyoshiharu/ai-dating-proposer/controller"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
 )
 
+type BingAnswer struct {
+	Value []struct {
+		ContentURL string `json:"contentUrl"`
+	} `json:"value"`
+}
+
 func main() {
-	router := controller.NewRouter()
-	router.Run()
+	query := "猫"
+	maxImageSize := "10"
+
+	endpoint := "https://api.bing.microsoft.com/v7.0/images/search?mkt=ja-JP&count=" + maxImageSize + "&q=" + url.QueryEscape(query)
+	token := os.Getenv("BING_API_TOKEN")
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Set("Ocp-Apim-Subscription-Key", token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	resbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	ans := new(BingAnswer)
+	err = json.Unmarshal(resbody, &ans)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Print("Output of BingAnswer: \r\n\r\n")
+
+	for _, v := range ans.Value {
+		fmt.Println("-------------------------------------")
+		fmt.Println(v.ContentURL)
+	}
 }
