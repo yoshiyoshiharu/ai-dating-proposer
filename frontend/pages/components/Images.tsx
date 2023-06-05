d8edb48c4923dc374454116dcf3e3659eb04bf8e
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-06-05 09:37:20 +0900

    読み込みに失敗した画像は表示しない

--- frontend/pages/components/Images.tsx
+++ frontend/pages/components/Images.tsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
+
 const Images = ({ imageUrls }: { imageUrls: string[] }) => {
+  const [errorImages, setErrorImages] = useState<number[]>([]);
+
+  const handleImageError = (imageIndex: number) => {
+    setErrorImages(prevErrorImages => [...prevErrorImages, imageIndex]);
+  };
+
   return (
     <>
       <div className="images">
-        {imageUrls !== undefined && imageUrls.length > 0 && imageUrls.map((imageUrl) => (
-          <img className="image" src={imageUrl} key={imageUrl} />
+        {imageUrls !== undefined && imageUrls.length > 0 && imageUrls.map((imageUrl, index) => (
+          !errorImages.includes(index) &&
+          <img className="image" src={imageUrl} key={index} alt="建物写真" onError={() => handleImageError(index)} />
         ))}
       </div>
       <style jsx>{`
