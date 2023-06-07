9c7354475b1e875cb7e8a92bd408327c8599046a
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2023-06-07 20:32:17 +0900

    プランのデザイン

--- frontend/pages/components/Plans.tsx
+++ frontend/pages/components/Plans.tsx
@@ -3,6 +3,8 @@ import Images from './Images';
 import Link from 'next/link';
 import Share from './Share';
 import { Spot } from '../../entity/spot';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faClockFour } from '@fortawesome/free-solid-svg-icons';
 
 export default function Plans({ spot }: { spot: Spot }) {
   return (
@@ -20,12 +22,15 @@ export default function Plans({ spot }: { spot: Spot }) {
             <p className='error-message'>プランが見つかりませんでした。もう一度試してください。</p>
           }
           <Share></Share>
+          <ul className="time-schedule">
           {spot.plans.length > 0 && spot.plans.map((plan: Plan) => (
-            <div key="plan.time">
-              <p className='title'>{plan.time} : {plan.plan}</p>
-              <p className='description'>{plan.description}</p>
-            </div>
+              <li>
+                <span className="time">{plan.time}</span>
+                <div className="sch_box"><p className="sch_title">{plan.plan}</p>
+              </div>
+            </li>
           ))}
+          </ul>
         </div>
       }
       <style jsx>{`
@@ -35,18 +40,58 @@ export default function Plans({ spot }: { spot: Spot }) {
           padding: 20px;
           border-radius: 10px;
         }
-        .plan-header {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
+        .time-schedule {
+          width: 80%;
+          list-style: none;
+          margin: 3rem auto 0 6em;
+          padding-left: 20px;
+          border-left: 6px solid #a7be18;
+          box-sizing: border-box;
         }
-        .plan-title {
-          font-size: 1.2rem;
+        .time-schedule li {
+          width: 100%;
+          margin: 0 0;
+          padding: 5px 0;
+          position: relative;
         }
-        .title {
-          font-size: 1.4rem;
-          border-bottom: 1px solid #333;
-          color: #F77;
+        .time-schedule span.time {
+          width: 5em;
+          display: inline-block;
+          margin-left: -8em;
+          padding: 0 0 5px;
+          margin-top: 15px;
+          vertical-align: top;
+          position: relative;
+          text-align: right;
+          box-sizing: border-box;
+        }
+        .time-schedule span.time::after {
+          content: "";
+          position: absolute;
+          right: -35px;
+          top: 0;
+          background: #a7be18;
+          width: 20px;
+          height: 20px;
+          border-radius: 10px;
+        }
+        .time-schedule .sch_box {
+          display: inline-block;
+          width: 100%;
+          margin-left: 30px;
+          padding: 15px 10px 15px 10px;
+          vertical-align: middle;
+          background: #efefef;
+          box-sizing: border-box;
+          border-radius: 6px;
+        }
+        .time-schedule .sch_title {
+          font-size: 16px;
+          font-weight: 700;
+        }
+        .time-schedule .sch_tx {
+          font-size: 14px;
+          font-weight: normal;
         }
         .error-message {
           font-size: 0.8rem;
@@ -67,7 +112,7 @@ export default function Plans({ spot }: { spot: Spot }) {
           font-size: 1rem;
           margin: 10px;
         }
-     `}</style>
+      `}</style>
     </>
   )
 }
